13cdc06f1538a42f738da6096e65167768c452b6
WuddyS <jomyut7@gmail.com>
2015-10-06 08:49:16 +0700

    MaintenanaceModal
    uploadหน้าเพิ่มงานซ่อม

--- application/views/maintenance/maintenance_form_view.php
+++ application/views/maintenance/maintenance_form_view.php
@@ -11,5 +11,41 @@
             <h2 class="page-header"><?= $page_title ?><small><?= $page_title_small ?></small></h2>            
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-12">
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <label>หัวข้อ :</label>
+                    <?= $form['JobName'] ?>
+                    <?php echo form_error('JobName', '<font color="error">', '</font>'); ?>           
+                </div>
+                <div class="form-group">
+                    <label>สถานที่ :</label>
+                    <?= $form['RoomNo'] ?>
+                    <?php echo form_error('RoomNo', '<font color="error">', '</font>'); ?>           
+                </div>
+                <div class="form-group">
+                    <label>เลขที่กว. :</label>
+                    <?= $form['NumberKWDevice'] ?>
+                    <?php echo form_error('NumberKWDevice', '<font color="error">', '</font>'); ?>           
+                </div>
+                <div class="form-group">
+                    <label>แนบรูปภาพ :</label>
+                    <?= $form['ImageName'] ?>
+                    <?php echo form_error('ImageName', '<font color="error">', '</font>'); ?>           
+                </div>
+                <div class="form-group">
+                    <label>ปัญหาที่แจ้ง :</label>
+                    <?= $form['JobDetail'] ?>
+                    <?php echo form_error('JobDetail', '<font color="error">', '</font>'); ?>           
+                </div>
+                <div class="form-group">
+                    <label>หมายเหตุ :</label>
+                    <?= $form['Note'] ?>
+                    <?php echo form_error('Note', '<font color="error">', '</font>'); ?>           
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
